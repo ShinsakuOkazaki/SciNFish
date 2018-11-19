129fc727f9abab70a902074e33a5cd876cae2d70
sinsakuokazaki <golisaku4639@gmail.com>
2018-11-19 16:30:22 -0500

    added a all of field for sign up page. added a string list for security question in strings.xml.

--- app/src/main/java/com/gai/guras/scinfish/SignUpActivity.java
+++ app/src/main/java/com/gai/guras/scinfish/SignUpActivity.java
@@ -3,14 +3,22 @@ package com.gai.guras.scinfish;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText usernameInput;
     private Button submitButton;
     private EditText passwordInput;
+    private EditText password2Input;
+    private Spinner securityQ1Selection;
+    private Spinner securityQ2Selection;
+    private ArrayAdapter<CharSequence> adapter ;
+    private EditText answer1Input;
+    private EditText answer2Input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,16 @@ public class SignUpActivity extends AppCompatActivity {
         usernameInput = findViewById(R.id.usernameInput);
         submitButton = findViewById(R.id.buttonSubmit);
         passwordInput = findViewById(R.id.passwordInput);
+        password2Input = findViewById(R.id.password2Input);
+        securityQ1Selection = findViewById(R.id.securityQ1Selection);
+        securityQ2Selection = findViewById(R.id.securityQ2Selection);
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.security_question, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        securityQ1Selection.setAdapter(adapter);
+        securityQ2Selection.setAdapter(adapter);
+        answer1Input = findViewById(R.id.answer1Input);
+        answer2Input = findViewById(R.id.answer2Input);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
